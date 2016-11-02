4fd961f9f0cc05a47cb6d148f06bb1612119965e
Kimberleydillon <kim.alexandra.dillon@gmail.com>
2016-11-02 22:29:33 +0000

    Shows Css display and basic first react components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,44 @@
 import React, {Component} from 'react';
+import Message from './Message.jsx';
+import MessageList from './MessageList.jsx';
+import ChatBar from './ChatBar.jsx';
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+        <div class="wrapper">
+          <nav>
+            <h1>Shut up, Kyle</h1>
+            </nav>
+          <div id="message-list">
+          <MessageList />
+          <Message/>
+      </div>
+      <ChatBar/>
+        </div>
     );
   }
 }
 export default App;
+//The index.jsx file mounts our App component onto a DOM element, in this case, react-root.
+
+// ES6 way using class
+//class App extends React.Component {
+  //render() {
+    // .. code in the render function
+  //}
+//}
+
+// var data = {
+//   currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+//   messages: [
+//     {
+//       username: "Bob",
+//       content: "Has anyone seen my marbles?",
+//     },
+//     {
+//       username: "Anonymous",
+//       content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+//     }
+//   ]
+// };
--- src/index.jsx
+++ src/index.jsx
@@ -9,3 +9,7 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('react-root'));
+
+
+// write code for client app in jsx files.
+//clients are always connecting and disconnecting.
